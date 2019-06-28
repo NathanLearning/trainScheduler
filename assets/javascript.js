31d9512de2a9e462ece55584822ec10f2e006293
Nathan Visti <nathanvisti@gmail.com>
2019-06-28 13:23:40 -0400

    Linked database in html.  Redone time logic

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,3 +1,5 @@
+$(document).ready(function(){
+
 var firebaseConfig = {
     apiKey: "AIzaSyBJY3Jwq0EhAbRtoESRx6Fs0CuJkvLQYAQ",
     authDomain: "trainscheduler-6075a.firebaseapp.com",
@@ -33,5 +35,48 @@ $("#add-train-btn").on("click", function(event) {
     $("#rate-input").val("")
 
     return false
+})
+
+database.ref().on("child_added", function(snapshot){
+    newRow = $("<tr>");
+    newRow.append("<td>" + snapshot.val().trainName + "</th>")
+    newRow.append("<td>" + snapshot.val().trainDestination + "</th>")
+    newRow.append("<td>" + snapshot.val().trainFrequency + "</th>")
+
+    var start = snapshot.val().trainTime
+    var frequency = snapshot.val().trainFrequency
+    var now = moment().format("HH:mm")
+    var next = ""
+
+    var timeDifference = moment().diff(moment(start, "HH:mm"), "minutes")
+
+    if (timeDifference > 0) {
+        next = moment(start, "HH:mm").format("hh:mm a")
+        minutesUntil = moment(start, "HH:mm").diff(moment(), "minutes")
+    }
+
+    else {
+        minutesSince = timeDifference % frequency
+        minutesUntil = frequency - minutesSince
+        next = moment().add(minutesUntil, "m").format("h:mm a")
+    }
+
+    if (minutesUntil >= 60) {
+        a = minutesUntil
+        var hours = Math.trunc(a/60)
+        var minutes = a % 60
+        minutesUntil = hours + " hr " + minutes + " min"
+    }
+
+    else {
+        minutesUntil = minutesUntil + " min"
+    }
+
+    newRow.append("<td>" + next + "</td>")
+    newRow.append("<td>" + minutesUntil + "</td>")
+
+    $("#trainTable").append(newRow)
+
+})
 
 })
